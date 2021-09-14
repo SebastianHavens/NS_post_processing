8a0c9602adb6b5f1335e95b1dc849017d2ba18a8
Sebastian Havens <sebastian.havens@outlook.com>
2021-09-14 10:51:27 +0100

    Fixed columns in output so that all data is now in the correct location

--- H_T_extrapolate.py
+++ H_T_extrapolate.py
@@ -4,20 +4,21 @@ import numpy as np
 import sys
 from scipy import interpolate
 
-analyse_file = 'pf'
-energy_file  = sys.argv[1]
-ke_file      = sys.argv[2]
+analyse_file = sys.argv[1]
+energy_file  = sys.argv[2]
+ke_file      = sys.argv[3]
 
 try:
     T, U = np.loadtxt(str(analyse_file), comments='#', usecols=(0,3), unpack=True)
 except:
-    print('pf does not exit')
+    print('analysis file does not exist')
     exit()
 
 energy = np.loadtxt(str(energy_file))
 ke     = np.loadtxt(str(ke_file))
 
 U2=[]
+
 for x in range(len(energy)):
     U2.append(energy[x] - ke[x])
 
@@ -26,9 +27,6 @@ U2 = np.delete(U2, np.argwhere(U2 < np.amin(U)))
 
 f = interpolate.interp1d(U,T)
 
-
 h_temp = open('temp.temp', 'w')
-h_U = open('U.temp', 'w')
-for x in range(len(U2) ) :
+for x in range(len(U2)) :
     h_temp.write(str(f(U2[x])) +  '\n')
-    h_U.write(str(U2[x]) +'\n')
--- qw_traj.sh
+++ qw_traj.sh
@@ -7,8 +7,14 @@
 #  argument 2 = character after traj.   #this is the processer number first processor you want to analyse
 #  argument 3 = last processor number you want to analyse
 
-ns_analyse $1.energies -M 0.1 -n 500 -D 5 > pf
-echo 'Energy:    Volume:    q4:      w4:    q6:    w6:  iteration:   Temp:  U:' > $1-$2-$3.qw46HV
+# ~/pymatnest/ns_analyse $1.energies -M 0.1 -n 500 -D 5 > analyse.dat
+
+#Extract min. and max. energies from analysis file for later file truncation. "sort" orders the file according to column 4 (-nk4,4), then head and tail extracts first and last lines, then awk extracts only column 4.
+U_min=$(sort -nk4,4 analyse.dat | head -n 1 | awk '{print $4}')
+U_max=$(sort -nk4,4 analyse.dat | tail -n 1 | awk '{print $4}')
+
+#Column headers
+echo 'Temp:	Total energy:	Kinetic energy:	Volume:	q4:	w4:	q6:	w6:	iteration:' > $1-$2-$3.qw46HV
 
 for iter in $(seq $2 $3)
 do	
@@ -16,22 +22,28 @@ do
 	get_qw atfile_in=$1.traj.$iter.extxyz r_cut=3 l=4 calc_QWave=T  > $1.traj.$iter.qw4
 	get_qw atfile_in=$1.traj.$iter.extxyz r_cut=3 l=6 calc_QWave=T  > $1.traj.$iter.qw6
 	
+	#Extract data lines from QW output
+	grep "[[:digit:]]\.[[:digit:]].*[[:digit:]]\.[[:digit:]]" $1.traj.$iter.qw4 > $1.qw4_temp
+	grep "[[:digit:]]\.[[:digit:]].*[[:digit:]]\.[[:digit:]]" $1.traj.$iter.qw6 > $1.qw6_temp
 	
-	tail -n+12 $1.traj.$iter.qw4 | head -n-3 > $1.qw4_temp
-	tail -n+12 $1.traj.$iter.qw6 | head -n-3 > $1.qw6_temp
+	# grep the energies, ke,  volumes and iteration numbers from the traj files in neat columns and create temporary files:
+	grep -o ns_energy=.[[:digit:]]*\.[[:digit:]]* $1.traj.$iter.extxyz | sed "s/ns_energy=//g" > $1.ener_temp
+	grep -o ns_KE=.[[:digit:]]*\.[[:digit:]]* $1.traj.$iter.extxyz | sed "s/ns_KE=//g" > $1.ke_temp
+	grep -o volume=.[[:digit:]]*\.[[:digit:]]* $1.traj.$iter.extxyz | sed "s/volume=//g" > $1.vol_temp
+	grep -o iter=.[[:digit:]]*\.[[:digit:]]* $1.traj.$iter.extxyz | sed "s/iter=//g" > $1.iter_temp
 	
-	# grep the energies, ke,  volumes and iteration numbers from the traj files in neat columns and creat temporary files:
-	grep -o ns_energy=.[[:digit:]]*\.[[:digit:]]* $1.traj.$iter.extxyz | sed "s/ns_energy=//g" >> $1.ener_temp
-	grep -o volume=.[[:digit:]]*\.[[:digit:]]* $1.traj.$iter.extxyz | sed "s/volume=//g" >> $1.vol_temp
-	grep -o iter=.[[:digit:]]*\.[[:digit:]]* $1.traj.$iter.extxyz | sed "s/iter=//g" >> $1.iter_temp
-	grep -o ns_KE=.[[:digit:]]*\.[[:digit:]]* $1.traj.$iter.extxyz | sed "s/ns_KE=//g" >> $1.ke_temp
+	# Interpolate data to calculate temperature dependence
+        H_T_extrapolate.py analyse.dat $1.ener_temp $1.ke_temp
 	
-	
-	H_T_extrapolate.py $1.ener_temp $1.ke_temp
-	
-	#grep the energies, volume, Q and W data from the two files and create a summary result file, neatly arranging them by columns
-	pr -m -t -s $1.ener_temp $1.vol_temp $1.qw4_temp $1.qw6_temp $1.iter_temp temp.temp U.temp| awk '{print $1,$2,$3,$4,$5,$6,$7,$8, $9 }' >> $1-$2-$3.qw46HV
-	
+	#Combine data files (using paste, where -d flag is used seperate columns with tabs), then use awk to extract lines that have energies within range given by analysis file
+	paste -d "\t" $1.ener_temp $1.ke_temp $1.vol_temp $1.qw4_temp $1.qw6_temp $1.iter_temp | awk -v a="$U_min" '$1-$2 >= a' | awk -v a="$U_max" '$1-$2 <= a' > $1.all_temp
+
+	#Add temperatures to data file
+	paste -d "\t" temp.temp $1.all_temp >> $1-$2-$3.qw46HV
+
+	#Seperate each data block with empty line
+	echo "" >> $1-$2-$3.qw46HV	
+
 	# remove the temporary files
 	rm $1.ener_temp
 	rm $1.vol_temp
@@ -40,7 +52,7 @@ do
 	rm $1.iter_temp
 	rm temp.temp
 	rm $1.ke_temp
-	rm U.temp
+	rm $1.all_temp
 done
 
 mkdir qw46
@@ -48,3 +60,4 @@ mkdir qw46
 mv *.qw4 qw46
 mv *.qw6 qw46
 mv *.idx qw46
+
