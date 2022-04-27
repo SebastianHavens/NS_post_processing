34e784bf25d2c8faedce6ac984ea31dc197a5f23
Sebastian Havens <sebastian.havens@outlook.com>
2022-04-27 15:22:57 +0100

    clean up

--- qw_traj.sh
+++ qw_traj.sh
@@ -1,24 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
 #files need: .energies
 #            .traj 
 
-#  argument 1 = charactrers before .traj 
-#  argument 2 = character after traj.   #this is the processer number first processor you want to analyse
+#  argument 1 = characters before .traj
+#  argument 2 = character after traj.   #this is the processor number first processor you want to analyse
 #  argument 3 = last processor number you want to analyse
 #  argument  4 = start temperature
 #  argument  5 = Number of temperatures
 #  argument  6 - difference between temperatures
 
-if [ $# -ne 6 ]; then
+
+if [ $# -lt 6 ]; then
     echo "Not all arguments provided"
     echo ""
-    echo "  argument  1 = charactrers before .traj "
-    echo "  argument  2 = character after traj.   #this is the processer number first processor you want to analyse"
+    echo "  argument  1 = characters before .traj "
+    echo "  argument  2 = character after traj.   #this is the processor number first processor you want to analyse"
     echo "  argument  3 = last processor number you want to analyse"
     echo "  argument  4 = start temperature"
     echo "  argument  5 = Number of temperatures"
     echo "  argument  6 = difference between temperatures"
+    echo "  argument  7 = optional, -k value"
     exit 1
 fi
 
@@ -26,9 +28,17 @@ fi
 mkdir qw46
 mkdir rdf
 
-ns_analyse $1.energies -M $4 -n $5 -D $6 > analyse.dat
+
+if [ -z "$7" ]
+  then
+    ns_analyse "$1".energies -M "$4" -n "$5" -D "$6" > analyse.dat
+  else
+     ns_analyse "$1".energies -M "$4" -n "$5" -D "$6" -k "$7" > analyse.dat
+fi
+
+
 echo 'Energy:    Volume:    q4:      w4:    q6:    w6:  iteration:   Temp:  U:' > $1-$2-$3.qw46HV
-for iter in $(seq $2 $3)
+for iter in $(seq "$2" "$3")
 do	
 	echo 'Processor number:'  $iter
 	get_qw atfile_in=$1.traj.$iter.extxyz r_cut=3 l=4 calc_QWave=T print_QWxyz=T  > $1.traj.$iter.qw4
@@ -42,13 +52,12 @@ do
 	tail -n+12 $1.traj.$iter.qw6 | head -n-3 > $1.qw6_temp
 	
 	# grep the energies, ke,  volumes and iteration numbers from the traj files in neat columns and creat temporary files:
-	grep -o ns_energy=.[[:digit:]]*\.[[:digit:]]* $1.traj.$iter.extxyz | sed "s/ns_energy=//g" >> $1.$iter.ener_temp
-	grep -o volume=.[[:digit:]]*\.[[:digit:]]* $1.traj.$iter.extxyz | sed "s/volume=//g" >> $1.$iter.vol_temp
-	grep -o iter=.[[:digit:]]*\.[[:digit:]]* $1.traj.$iter.extxyz | sed "s/iter=//g" >> $1.$iter.iter_temp
-	grep -o ns_KE=.[[:digit:]]*\.[[:digit:]]* $1.traj.$iter.extxyz | sed "s/ns_KE=//g" >> $1.$iter.ke_temp
+	grep -o "ns_energy=.[[:digit:]]*\.[[:digit:]]*" $1.traj.$iter.extxyz | sed "s/ns_energy=//g" >> $1.$iter.ener_temp
+	grep -o "volume=.[[:digit:]]*\.[[:digit:]]*" $1.traj.$iter.extxyz | sed "s/volume=//g" >> $1.$iter.vol_temp
+	grep -o "iter=.[[:digit:]]*" "$1".traj."$iter".extxyz | sed "s/iter=//g" >> $1.$iter.iter_temp
+	grep -o "ns_KE=.[[:digit:]]*\.[[:digit:]]*" $1.traj.$iter.extxyz | sed "s/ns_KE=//g" >> $1.$iter.ke_temp
 	
-	
-	H_T_extrapolate.py analyse.dat $1.$iter.ener_temp $1.$iter.ke_temp
+	./H_T_extrapolate.py analyse.dat "$1".$iter.ener_temp "$1".$iter.ke_temp
 	
 	#grep the energies, volume, Q and W data from the two files and create a summary result file, neatly arranging them by columns
 	pr -m -t -s $1.$iter.ener_temp $1.$iter.vol_temp $1.qw4_temp $1.qw6_temp $1.$iter.iter_temp temp.temp U.temp| awk '{print $1,$2,$3,$4,$5,$6,$7,$8, $9 }' >> $1-$2-$3.qw46HV
@@ -64,8 +73,7 @@ do
 	mv allrdf.out  allrdf.$iter.out
 	
 done
-
-weighted_rdf.py $1 $2 $3 $4 $5 $6 
+./weighted_rdf.py $1 $2 $3 $4 $5 $6
 
 mv *.qw4 qw46
 mv *.qw6 qw46
