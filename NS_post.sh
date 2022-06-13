8f514d4e2b08ab0e1304239b9bad02238c6f70b8
Sebastian Havens <sebastian.havens@outlook.com>
2022-06-13 16:36:50 +0100

    Fixed issue with incorrect file name

--- NS_post.sh
+++ NS_post.sh
@@ -25,37 +25,34 @@ do
 	get_qw atfile_in=$prefix.traj."$iter".extxyz r_cut="$qw_r_cut" l=6 calc_QWave=T print_QWxyz=T > $prefix.traj."$iter".qw6
 	
 	#Extract data lines from QW output
-	grep "[[:digit:]]\.[[:digit:]].*[[:digit:]]\.[[:digit:]]" "$prefix".traj."$iter".qw4 > "$prefix".qw4_temp
-	grep "[[:digit:]]\.[[:digit:]].*[[:digit:]]\.[[:digit:]]" "$prefix".traj."$iter".qw6 > "$prefix".qw6_temp
+	grep "[[:digit:]]\.[[:digit:]].*[[:digit:]]\.[[:digit:]]" "$prefix".traj."$iter".qw4 > "$prefix"."$iter".qw4.temp
+	grep "[[:digit:]]\.[[:digit:]].*[[:digit:]]\.[[:digit:]]" "$prefix".traj."$iter".qw6 > "$prefix"."$iter".qw6.temp
 	
-	tail -n+12 "$prefix".traj."$iter".qw4 | head -n-3 > "$prefix".qw4_temp
-	tail -n+12 "$prefix".traj."$iter".qw6 | head -n-3 > "$prefix".qw6_temp
+	tail -n+12 "$prefix".traj."$iter".qw4 | head -n-3 > "$prefix".qw4.temp
+	tail -n+12 "$prefix".traj."$iter".qw6 | head -n-3 > "$prefix".qw6.temp
 	
 	# grep the energies, ke,  volumes and iteration numbers from the traj files in neat columns and creat temporary files:
-	grep -o "ns_energy=.[[:digit:]]*\.[[:digit:]]*" "$prefix".traj."$iter".extxyz | sed "s/ns_energy=//g" >> "$prefix"_"$iter"_ener_temp
-	grep -o "volume=.[[:digit:]]*\.[[:digit:]]*" "$prefix".traj."$iter".extxyz | sed "s/volume=//g" >> "$prefix"."$iter".vol_temp
+	grep -o "ns_energy=.[[:digit:]]*\.[[:digit:]]*" "$prefix".traj."$iter".extxyz | sed "s/ns_energy=//g" >> "$prefix"_"$iter"_ener.temp
+	grep -o "volume=.[[:digit:]]*\.[[:digit:]]*" "$prefix".traj."$iter".extxyz | sed "s/volume=//g" >> "$prefix"."$iter".vol.temp
 	grep -o "iter=.[[:digit:]]*" "$prefix".traj."$iter".extxyz | sed "s/iter=//g" >> "$prefix"_"$iter"_iter.temp
-	grep -o "ns_KE=.[[:digit:]]*\.[[:digit:]]*" "$prefix".traj."$iter".extxyz | sed "s/ns_KE=//g" >> "$prefix"_"$iter"_ke_temp
+	grep -o "ns_KE=.[[:digit:]]*\.[[:digit:]]*" "$prefix".traj."$iter".extxyz | sed "s/ns_KE=//g" >> "$prefix"_"$iter"_ke.temp
 
-	H_T_extrapolate.py analyse.dat "$prefix"_"$iter"_ener_temp "$prefix"_"$iter"_ke_temp
+	H_T_extrapolate.py analyse.dat "$prefix"_"$iter"_ener.temp "$prefix"_"$iter"_ke.temp
 	
 	#grep the energies, volume, Q and W data from the two files and create a summary result file, neatly arranging them by columns
-	pr -m -t -s "$prefix"_"$iter"_ener_temp "$prefix"."$iter".vol_temp "$prefix".qw4_temp "$prefix".qw6_temp "$prefix"_"$iter"_iter.temp temp.temp U.temp| awk '{print $prefix,$proc_start,$proc_end,$start_temp,$num_temp,$delta_temp,$7,$8, $9 }' >> $prefix-$proc_start-$proc_end.qw46HV
+	pr -m -t -s "$prefix"_"$iter"_ener.temp "$prefix"."$iter".vol.temp "$prefix"."$iter".qw4.temp "$prefix"."$iter".qw6.temp "$prefix"_"$iter"_iter.temp temp.temp U.temp| awk '{print $prefix,$proc_start,$proc_end,$start_temp,$num_temp,$delta_temp,$7,$8, $9 }' >> $prefix-$proc_start-$proc_end.qw46HV
 
 
 
-	rdf xyzfile="$prefix".traj."$iter".extxyz datafile=foo mask1="$atom_type" mask2="$atom_type" r_cut="$rdf_r_cut" bin_width="$bin_width" >> NS_post.out
+	rdf xyzfile="$prefix".traj."$iter".extxyz datafile=foo.temp mask1="$atom_type" mask2="$atom_type" r_cut="$rdf_r_cut" bin_width="$bin_width" >> NS_post.out
 
   # Collate files for weighted RDF
 	cat allrdf.out >> collated_rdf.temp
-	cat "$prefix"_"$iter"_ener_temp >> collated_ener.temp
+	cat "$prefix"_"$iter"_ener.temp >> collated_ener.temp
 	cat "$prefix"_"$iter"_iter.temp >> collated_iter.temp
-
-	mv allrdf.out  allrdf."$iter".out
+  mv allrdf.out  allrdf."$iter".out
 
 	# remove the temporary files
-	rm $prefix.qw4_temp
-	rm $prefix.qw6_temp
 	rm temp.temp
 	rm U.temp
 
@@ -85,9 +82,8 @@ NS_weighted_rdf.exe < w_rdf_param.temp >> w_rdf.out
 mv *.qw4 qw46
 mv *.qw6 qw46
 mv *.idx qw46
-rm *_temp
-rm foo collated_rdf.temp collated_iter_ener.temp collated_iter.temp collated_ener.temp w_rdf_param.temp
 mv qw4_$prefix.traj.*.extxyz qw46
 mv qw6_$prefix.traj.*.extxyz qw46
 mv allrdf.*.out rdf
+rm *.temp
 
